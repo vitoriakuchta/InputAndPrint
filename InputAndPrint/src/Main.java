c1216240f1d517f0b4884d34a46cedc080ed52db
vitoriakuchta <vitoria123kuchta@gmail.com>
2024-08-12 10:11:59 -0300

    Add files via upload

---
+++ InputAndPrint/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int valor1 = scan.nextInt();
+        int valor2 = scan.nextInt();
+        int valor3 = scan.nextInt();
+
+        System.out.println(valor1);
+        System.out.println(valor2);
+        System.out.println(valor3);
+    }
+
+}
